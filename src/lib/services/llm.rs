2412e33a348839ee9c33824bcfe89245ae7e7e09
Mohamed Aouadhi <med.aouadhi@gmail.com>
2024-01-17 10:56:48 +0100

    add missing arg

--- src/lib/services/llm.rs
+++ src/lib/services/llm.rs
@@ -118,6 +118,7 @@ impl Agent for OpenAiModel {
             embeddings,
             None,
             None,
+            None,
         );
 
         let doc_dog_definition = r#"The dog (Canis familiaris[4][5] or Canis lupus familiaris[5]) is a domesticated descendant of the wolf. Also called the domestic dog, it is derived from the extinct Pleistocene wolf,[6][7] and the modern wolf is the dog's nearest living relative.[8] Dogs were the first species to be domesticated[9][8] by hunter-gatherers over 15,000 years ago[7] before the development of agriculture.[1] Due to their long association with humans, dogs have expanded to a large number of domestic individuals[10] and gained the ability to thrive on a starch-rich diet that would be inadequate for other canids.[11]
